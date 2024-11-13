4ae4f706888c4468123db9048f979fe3d18030a8
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-11-13 21:45:27 +0200

    Homework 6: Add implementation on plotting random dimensions

--- Seminarul 6/src/main.py
+++ Seminarul 6/src/main.py
@@ -89,5 +89,43 @@ def pick_dimensions(vectorized_words, k=6):
     return sample(range(0, vectorized_words[0].shape[0]), k=k)
 
 
+def plot_random_dimensions(
+    words: list[str], vectorized_words: list[ndarray], dimensions: list[int]
+):
+    xs, ys, zs, reds, greens, blues = tuple(
+        array([vectorized_word[dimension] for vectorized_word in vectorized_words])
+        for dimension in dimensions
+    )
+    reds = scale_between_0_and_1(reds)
+    blues = scale_between_0_and_1(blues)
+    greens = scale_between_0_and_1(greens)
+    colors = stack((reds, blues, greens), axis=-1)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    ax.scatter(xs, ys, zs, marker="o", c=colors)
+
+    for i, word in enumerate(words):
+        if i >= len(vectorized_words):
+            break
+
+        ax.text(xs[i], ys[i], zs[i], word, size=12, zorder=1, color="k")
+
+    ax.set_xlabel(f"Dimension {dimensions[0]}")
+    ax.set_ylabel(f"Dimension {dimensions[1]}")
+    ax.set_zlabel(f"Dimension {dimensions[2]}")
+    ax.set_title("Random dimensions plot")
+
+    fig.savefig(
+        f"./outputs/random_dimensions({", ".join([str(dimension) for dimension in dimensions])}).png"
+    )
+    fig.show()
+    fig.waitforbuttonpress()
+
+
+def scale_between_0_and_1(vector: ndarray):
+    return (vector - min(vector)) / (max(vector) - min(vector))
+
+
 if __name__ == "__main__":
     main()
